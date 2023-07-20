de235d5fda5ed9657697b8821e5d1d9746befe29
Meghaatyagi <138965666+Meghaatyagi@users.noreply.github.com>
2023-07-20 11:46:48 +0530

    Update P.java

--- P.java
+++ P.java
@@ -1,8 +1,8 @@
-public class D {
+public class P {
     public static void main(String[] args) {
-        String a = "Amit";
-        String b = "Amit";
-        String c = "Amit";
+        String a = "Megha";
+        String b = "Megha";
+        String c = "Megha";
         c = "Ram"; // Immutable
 
         StringBuffer sb = new StringBuffer(); // Mutable 
